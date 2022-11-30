f9fe833ce05f91d56edfbaca2b475ebfb1c48a5c
Dmitry Kizimov <kizimov@mail.ru>
2022-12-01 00:07:00 +0300

    Task 3 is working

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Even / odd digit
+Console.WriteLine("Введите число:");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit % 2 == 0)
+    Console.WriteLine("Вы ввели чётное число");
+else
+    Console.WriteLine("Вы ввели нечётное число");
+    
