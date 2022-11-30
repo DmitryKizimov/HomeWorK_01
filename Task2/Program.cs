c8da10fefa455e1b38aaa1606d6af01f112160c9
Dmitry Kizimov <kizimov@mail.ru>
2022-11-30 23:53:26 +0300

    Task 2 OK

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Task 2 - Three digit input, that shows maximum
+int[] digits = new int[3]; 
+
+Console.Write("Введите первое число:");
+digits[0] = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+digits[1] = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число:");
+digits[2] = int.Parse(Console.ReadLine());
+
+int maximum = digits[0];
+int i = 1;
+
+while (i < 3)
+{
+    if (digits[i] > maximum)
+    maximum = digits[i];
+    i++;
+}
+
+Console.WriteLine("Наибольшее число из трёх, введёных вами: ");
+Console.WriteLine(maximum);
