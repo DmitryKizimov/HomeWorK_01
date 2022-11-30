34dea0e8171bb0557ba6e043ef3e2d5e12137027
Dmitry Kizimov <kizimov@mail.ru>
2022-12-01 00:34:19 +0300

    Task 4 is working too

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// N - digit input, even digits output from 1 to N
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i < N)
+{
+    Console.WriteLine(i + 1);
+    i += 2;
+}
